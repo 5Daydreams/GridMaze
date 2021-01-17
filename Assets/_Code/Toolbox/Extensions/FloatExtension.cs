16bc9c89edd49344d0cf0a7ae22d688b144fcb26
Kiyoshi <nelsonkossuga@gmail.com>
2021-01-17 14:47:43 -0300

    Rearranged the float.ConvertToTimer() method;

--- Assets/_Code/Toolbox/Extensions/FloatExtension.cs
+++ Assets/_Code/Toolbox/Extensions/FloatExtension.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace _Code.Toolbox.Extensions
 {
@@ -6,43 +7,46 @@ namespace _Code.Toolbox.Extensions
     {
         public static string ConvertSecondsToTimer(this float self)
         {
+            string timerDisplay = "";
+
             int hours = 0;
-            string zeroShiftMinutes = "";
-            int minutes = 0;
-            string zeroShiftSeconds = "";
-            int seconds = 0;
-            int miliseconds = 0;
-        
             while (self / 3600.0f >= 1) // if there`s more than 3600 seconds within the timer
             {
                 self -= 3600.0f;
                 hours += 1;
             }
-        
+            if (hours != 0)
+                timerDisplay += hours.ToString() + ":";
+
+
+            string zeroShiftMinutes = "";
+            int minutes = 0;
             while (self / 60.0f >= 1) // if there`s more than 60 seconds within the timer
             {
                 self -= 60.0f;
                 minutes += 1;
             }
-        
             if (minutes < 10)
                 zeroShiftMinutes = "0";
-        
+
+            string zeroShiftSeconds = "";
+            int seconds = 0;
             seconds = Mathf.FloorToInt(self);
-            
             if (seconds < 10)
                 zeroShiftSeconds = "0";
-        
-            miliseconds = Mathf.FloorToInt((self - seconds)* 1000);
-            var miliString = miliseconds.ToString();
 
+            int miliseconds = 0;
+            miliseconds = Mathf.FloorToInt((self - seconds) * 1000);
+            var miliString = miliseconds.ToString();
             while (miliString.Length < 3)
             {
                 miliString = "0" + miliString;
             }
 
-            string timerDisplay = hours.ToString() + ":" + zeroShiftMinutes + minutes.ToString() + ":" + zeroShiftSeconds + seconds.ToString() + "." + miliString;
-            Debug.Log(timerDisplay);
+            timerDisplay += zeroShiftMinutes + minutes.ToString() + ":" 
+                            + zeroShiftSeconds + seconds.ToString() + "." 
+                            + miliString;
+
             return timerDisplay;
         }
     }
